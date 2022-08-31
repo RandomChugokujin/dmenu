adf356c946a1eb58791a21e3cb9a180f9660adc5
RandomChugokujin <RandomChugokujin@tutamail.com>
2022-08-31 10:48:14 -0400

    new file:   LICENSE 	new file:   Makefile 	new file:   README 	new file:   arg.h 	new file:   config.def.h 	new file:   config.h 	new file:   config.mk 	new file:   dmenu-alpha-20210605-1a13d04.diff 	new file:   dmenu-border-4.9.diff 	new file:   dmenu-center-20200111-8cd37e1.diff 	new file:   dmenu-linesbelowprompt-and-fullwidth-20211014.diff 	new file:   dmenu.1 	new file:   dmenu.c 	new file:   dmenu_path 	new file:   dmenu_run 	new file:   drw.c 	new file:   drw.h 	new file:   stest.1 	new file:   stest.c 	new file:   util.c 	new file:   util.h

---
+++ config.def.h
@@ -0,0 +1,37 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xf0;
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"terminus:size=12:style=bold:antialias=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d8dee9", "#2e3440" },
+	[SchemeSel] = { "#4c566a", "#81a1c1" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+}; //moved to dmenu.c
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 1;
+//moved to dmenu.c
